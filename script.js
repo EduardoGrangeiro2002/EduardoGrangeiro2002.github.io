ccdf29a6271989328248e1366e56c8d3d29055d3
EduardoGrangeiro2002 <76568887+EduardoGrangeiro2002@users.noreply.github.com>
2021-07-01 00:59:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,16 @@ function dell()
     let apagar = document.getElementById("Display").innerHTML
     document.getElementById("Display").innerHTML = apagar.substring(0, apagar.length -1);
 }
-function calculo(){
- let calcular = document.getElementById("Display").innerHTML;
- if(calcular)
- {
-     document.getElementById("Display").innerHTML = eval(calcular)
- }
 
+function calculo(){
+    
+    let calcular = document.getElementById("Display").innerHTML;
+ try{ 
+    if(calcular){
+     document.getElementById("Display").innerHTML = eval(calcular)
+    }
+   
+ }catch (error) {
+    document.getElementById("Display").innerHTML = "Syntaxe Error";
+}
 }
